fdbb528d264215925cd3f1d7f6ab371faf96c0f1
Milan Mazik <mazik1990@hotmail.com>
2023-10-23 20:30:40 +0200

    chips and potsize
    player and computer chips and potsize

--- Script/poker.js
+++ Script/poker.js
@@ -1,14 +1,17 @@
 const newGameButton = document.querySelector(".js-new-game-button");
-
 const playerCardsContainer = document.querySelector(
   ".js-player-cards-container"
 ); // space of player cards: bind variable with HTML section tag
 
-playerCardsContainer.innerHTML = "cards";
+const chipCountContainer = document.querySelector(".js-chip-count-container");
+const potContainer = document.querySelector(".js-pot-container");
 
 // program state
 let deckId = null;
 let playerCards = [];
+let playerChips = 100;
+let computerChips = 100;
+let pot = 0;
 
 function renderPlayerCards() {
   let html = "";
@@ -16,7 +19,29 @@ function renderPlayerCards() {
   for (let card of playerCards) {
     html += `<img src = "${card.image}" alt="${card.code}" />`;
   }
+  // instert 'html' into section:js-player-cards-container
   playerCardsContainer.innerHTML = html;
+}
+
+function renderChips() {
+  chipCountContainer.innerHTML = `
+  <div class="chip-count">Player: ${playerChips}</div>
+  <div class="chip-count">Computer: ${computerChips}</div>
+
+  `;
+}
+
+function renderPot() {
+  potContainer.innerHTML = `
+      <div class="chip-count">Pot: ${pot}</div>
+
+    `;
+}
+// rendering all datas at the same time. simplify of code > tiny speed loss
+function render() {
+  renderPlayerCards();
+  renderChips();
+  renderPot();
 }
 
 function drawAndRenderPlayerCards() {
@@ -25,7 +50,7 @@ function drawAndRenderPlayerCards() {
     .then((data) => data.json())
     .then(function (response) {
       playerCards = response.cards;
-      renderPlayerCards();
+      render();
     });
 }
 
@@ -38,3 +63,4 @@ function startGame() {
     });
 }
 newGameButton.addEventListener("click", startGame);
+render();
