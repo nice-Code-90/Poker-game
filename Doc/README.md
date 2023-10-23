4c1058787fdab5f628a7154e219d38a5faa57362
Milan Mazik <mazik1990@hotmail.com>
2023-10-23 17:59:58 +0200

    draw player cards
    Draw cards for one player

---
+++ Doc/README.md
@@ -0,0 +1,7 @@
+# Poker game using third party API
+
+## Third party tools used:
+
+-https:/deckofcardsapi.com
+
+-https://https://getcssscan.com/css-buttons-examples
--- Script/poker.js
+++ Script/poker.js
@@ -1,1 +1,40 @@
-alert("Hello Gambler.");
+const newGameButton = document.querySelector(".js-new-game-button");
+
+const playerCardsContainer = document.querySelector(
+  ".js-player-cards-container"
+); // space of player cards: bind variable with HTML section tag
+
+playerCardsContainer.innerHTML = "cards";
+
+// program state
+let deckId = null;
+let playerCards = [];
+
+function renderPlayerCards() {
+  let html = "";
+
+  for (let card of playerCards) {
+    html += `<img src = "${card.image}" alt="${card.code}" />`;
+  }
+  playerCardsContainer.innerHTML = html;
+}
+
+function drawAndRenderPlayerCards() {
+  if (deckId == null) return;
+  fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    .then((data) => data.json())
+    .then(function (response) {
+      playerCards = response.cards;
+      renderPlayerCards();
+    });
+}
+
+function startGame() {
+  fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+    .then((data) => data.json())
+    .then(function (response) {
+      deckId = response.deck_id;
+      drawAndRenderPlayerCards(); // todo: refactor async-await
+    });
+}
+newGameButton.addEventListener("click", startGame);
